2478e967a7f368c8eb333498e2e30ae8162da19b
iceberg20 <italoberg@yahoo.com.br>
2016-05-24 11:30:06 -0300

    Nova migração para adicionar um campo booleano que representa eventos bloqueados

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224033919) do
+ActiveRecord::Schema.define(version: 20160524142723) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "location"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160224033919) do
     t.integer  "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "place"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -39,6 +40,17 @@ ActiveRecord::Schema.define(version: 20160224033919) do
   add_index "comments", ["event_id"], name: "index_comments_on_event_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
+  create_table "eu_vous", force: :cascade do |t|
+    t.integer  "attendee_id"
+    t.integer  "attended_event_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  add_index "eu_vous", ["attended_event_id"], name: "index_eu_vous_on_attended_event_id"
+  add_index "eu_vous", ["attendee_id", "attended_event_id"], name: "index_eu_vous_on_attendee_id_and_attended_event_id", unique: true
+  add_index "eu_vous", ["attendee_id"], name: "index_eu_vous_on_attendee_id"
+
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -57,6 +69,7 @@ ActiveRecord::Schema.define(version: 20160224033919) do
     t.string   "local"
     t.integer  "category_id"
     t.string   "location"
+    t.boolean  "state"
   end
 
   add_index "events", ["category_id"], name: "index_events_on_category_id"
