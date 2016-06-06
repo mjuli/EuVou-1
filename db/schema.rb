b96bc20068df2f2acbe097354a071f04c040f72b
LuanGB <luan.goncbs@gmail.com>
2016-06-06 10:42:33 -0300

    especifidando comando para executar testes no travis

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524142723) do
+ActiveRecord::Schema.define(version: 20160606130109) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "location"
@@ -77,6 +77,17 @@ ActiveRecord::Schema.define(version: 20160524142723) do
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id"
+
+  create_table "reports", force: :cascade do |t|
+    t.integer  "reportee_id"
+    t.integer  "reported_event_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  add_index "reports", ["reported_event_id"], name: "index_reports_on_reported_event_id"
+  add_index "reports", ["reportee_id", "reported_event_id"], name: "index_reports_on_reportee_id_and_reported_event_id", unique: true
+  add_index "reports", ["reportee_id"], name: "index_reports_on_reportee_id"
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
