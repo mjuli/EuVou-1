051bd88e58ef336730a0d82b09c1d22eeccda0d8
iceberg20 <italoberg@yahoo.com.br>
2016-06-08 23:15:54 -0300

    1-A controller named Front was create to mange all views 2-Root route was changed to "Front#index"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  root 'front#index'
+
+  get 'front/index'
+
+  get 'front/show'
+
+  get 'front/edit'
+
+  get 'front/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
